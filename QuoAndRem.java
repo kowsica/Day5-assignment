b11ff083b1523460a2374e659bc02552d064acea
kowsica <kowsica97@gmail.com>
2023-01-25 09:09:22 +0530

    first commit

---
+++ QuoAndRem.java
@@ -0,0 +1,20 @@
+package assignment_day5;
+
+public class QuoAndRem {
+
+	public static void main(String[] args) {
+		double a=225;
+		double b=16;
+		double quo;
+		double rem;
+		quo=a/b;
+		rem=a%b;
+		System.out.println("The quotient is:" +quo);
+		System.out.println("The remainder is:" +rem);
+		
+		
+		
+
+	}
+
+}
