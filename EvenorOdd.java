ad328d1fd4280a3bcce5b1d7ea933574f75cf91d
kowsica <kowsica97@gmail.com>
2023-01-25 09:34:03 +0530

    firstcommit

---
+++ EvenorOdd.java
@@ -0,0 +1,21 @@
+package assignment_day5;
+import java.util.*;
+
+public class EvenorOdd {
+
+	public static void main(String[] args) {
+		int a;
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter a number:");
+		a=sc.nextInt();
+		if(a==0) {
+			System.out.println("The number"+" "+a +" " +"is even");
+		}
+			else {
+				System.out.println("The number"+" "+a +" " +"is odd");
+			}
+		}
+
+	}
+
+
